b634f2b19fd1e13cc98b3ad0fcf41efe492a2782
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-04-20 16:06:02 +0100

    Add connection to elephantSQL

--- simply_service/settings.py
+++ simply_service/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from django.contrib.messages import constants as messages
 
 if os.path.isfile('env.py'):
     import env
@@ -26,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&i$)#-d5=f7a%evqgn4p-rznv_!ek4x(_!=$xhiouk)83=i=7b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
