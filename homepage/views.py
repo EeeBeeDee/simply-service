e06a0f8f9056acd17d573f5de886f8dff5cb2d60
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-04-21 15:16:05 +0100

    Add templates dir, configure and create base.html

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 
-# Create your views here.
+
+
+
+def index(request):
+    """
+    Index view
+    """
+    return render(request, 'index.html')
