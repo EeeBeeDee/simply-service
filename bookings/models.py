9b4dd6588347387e7ee94ca060148e084aca2090
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-04-27 15:15:09 +0100

    Add bookings DB to admin portal

--- bookings/models.py
+++ bookings/models.py
@@ -13,3 +13,6 @@ class Bookings(models.Model):
     email = models.EmailField()
     number = models.CharField(max_length=20)
     slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
