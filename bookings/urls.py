f952bd8012c4a56460ee8d406b51a3ba6bf5e379
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-04-27 17:49:04 +0100

    Create view for booking detail and link urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.bookings, name='bookings'),
-    path('your_bookings/', views.your_bookings, name='your_bookings')
+    path('your_bookings/', views.your_bookings, name='your_bookings'),
+    path('<slug:slug>/', views.booking_detail, name='booking_detail'),
 
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -16,3 +16,8 @@ def bookings(request):
 def your_bookings(request):
     bookings = Bookings.objects.all()
     return render(request, 'your_bookings.html', {'bookings': bookings})
+
+def booking_detail(request, slug):
+    bookings = Bookings.objects.all()
+    booking = get_object_or_404(bookings, slug=slug)
+    return render(request, 'booking_detail.html', {'bookings': bookings})
