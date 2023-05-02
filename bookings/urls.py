6c49b7d18d4d3f715c0d8c029163116e78685ded
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-05-02 15:20:24 +0100

    Change to login mixin for class based views

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.bookings, name='bookings'),
-    path('your_bookings/', views.your_bookings, name='your_bookings'),
+    path('your_bookings/', views.YourBookings.as_view(), name='your_bookings'),
     path('<slug:slug>/', views.booking_detail, name='booking_detail'),
 
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic, View
 from django.contrib import messages
 from .models import Bookings
 from .forms import BookingsForm
 
 # Create your views here.
+
 
 @login_required()
 def bookings(request):
@@ -31,13 +33,21 @@ def bookings(request):
     return render(request, 'bookings.html', {'form': form})
 
 
-def your_bookings(request):
-    bookings = Bookings.objects.all()
-    return render(request, 'your_bookings.html', {'bookings': bookings})
+class YourBookings(LoginRequiredMixin,View):
+    
+    def get(self, request):
+        queryset = Bookings.objects.filter(name=request.user)
+        bookings = get_object_or_404(queryset)
+        
+        return render(request, 'your_bookings.html', {bookings: bookings})
+
+    # bookings = Bookings.objects.filter()
+    # return render(request, 'your_bookings.html', {'bookings': bookings})
+
 
 
 def booking_detail(request, slug):
-    bookings = Bookings.objects.all()
+    bookings = Bookings.objects.filter()
     booking = get_object_or_404(bookings, slug=slug)
 
     return render(request, 'booking_detail.html', {'booking': booking})
