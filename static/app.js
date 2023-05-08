9dfe0a7f1f247d5361a4f5051a5333dfa4b72855
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-05-08 17:51:20 +0100

    Add JS to collapse nav when scrolling to section

--- static/app.js
+++ static/app.js
@@ -1,5 +1,11 @@
 const alerts = document.getElementById('alerts')
+const restaurantLink = document.getElementById('restaurant-link')
+const navbar = document.querySelector('.navbar-collapse')
 
 setTimeout(() => {
     alerts.classList.add('hide')
 }, 4000);
+
+restaurantLink.addEventListener('click', () => {
+    navbar.classList.remove('show')  
+})
