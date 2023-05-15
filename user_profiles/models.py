2fef5eb527be8e97e526c6b437f5b2ecf972e11c
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-05-15 18:36:06 +0100

    Migrate Userprofile change of phone to CharField

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class UserProfile(AbstractUser):
-    phone = models.IntegerField(null=True)
+    phone = models.CharField(null=True, max_length=30)
 
     def __str__(self):
         return self.username
