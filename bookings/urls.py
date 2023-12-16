80b4ab3e72a69e71e7f4b7a151fa0ee3910aa5ba
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-12-16 21:11:16 +0000

    Clean up url names and links

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.bookings, name='bookings'),
-    path('<restaurant>', views.bookings, name='booking2name'),
+    path('<restaurant>', views.bookings, name='booking-name'),
     path('your_bookings/', views.YourBookings.as_view(), name='your_bookings'),
     path('<slug:slug>/', views.booking_detail, name='booking_detail'),
     path('update/<int:id>/', views.booking_update, name='booking_update'),
